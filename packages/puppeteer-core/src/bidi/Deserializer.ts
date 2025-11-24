495d69a4e7229522f5460cdeeaa159d5cd77af29
Maksim Sadym <sadym@chromium.org>
2025-11-24 16:54:13 +0100

    deserialize BiDi platform objects

--- packages/puppeteer-core/src/bidi/Deserializer.ts
+++ packages/puppeteer-core/src/bidi/Deserializer.ts
@@ -28,7 +28,14 @@ export class BidiDeserializer {
           return acc.add(this.deserialize(value));
         }, new Set());
       case 'object':
-        return result.value?.reduce((acc: Record<any, unknown>, tuple) => {
+        console.log(`!!@@## ${JSON.stringify(result)}`);
+        if (!result?.value) {
+          // Heuristic detecting platform objects. WebDriver BiDi serializes platform
+          // objects without value. Return an empty object, as there is no way to restore
+          // the original value's constructor.
+          return {};
+        }
+        return result.value.reduce((acc: Record<any, unknown>, tuple) => {
           const {key, value} = this.#deserializeTuple(tuple);
           acc[key as any] = value;
           return acc;
